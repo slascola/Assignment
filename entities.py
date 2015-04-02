5a771ed6e6e2a3cfb942f237b74f9327abaa91ca
Stephanie <slascola@calpoly.edu>
2015-04-01 23:00:13 -0700

    MinerNotFull Class added Methods

--- entities.py
+++ entities.py
@@ -19,6 +19,58 @@ class MinerNotFull:
       self.resource_count = 0
       self.animation_rate = animation_rate
       self.pending_actions = []
+   
+   def set_position(entity, point):
+      entity.position = point
+
+   def get_position(entity):
+      return entity.position
+
+   def get_images(entity):
+      return entity.imgs
+
+   def get_image(entity):
+      return entity.imgs[entity.current_img]
+
+   def get_rate(entity):     
+      return entity.rate     
+
+   def set_resource_count(entity, n):
+      entity.resource_count = n
+
+   def get_resource_count(entity):
+      return entity.resource_count
+
+   def get_resource_limit(entity):
+      return entity.resource_limit
+
+   def get_name(entity):              
+      return entity.name
+
+   def get_animation_rate(entity):
+      return entity.animation_rate
+
+   def remove_pending_action(entity, action):
+      if hasattr(entity, "pending_actions"):
+         entity.pending_actions.remove(action)
+
+   def add_pending_action(entity, action):
+      if hasattr(entity, "pending_actions"):
+         entity.pending_actions.append(action)
+
+   def get_pending_actions(entity):
+      if hasattr(entity, "pending_actions"):
+         return entity.pending_actions
+      else:
+         return []
+
+   def clear_pending_actions(entity):
+      if hasattr(entity, "pending_actions"):
+         entity.pending_actions = []
+
+   def next_image(entity):
+      entity.current_img = (entity.current_img + 1) % len(entity.imgs)
+
 
 class MinerFull:
    def __init__(self, name, resource_limit, position, rate, imgs,
@@ -91,13 +143,11 @@ class Quake:
       self.animation_rate = animation_rate
       self.pending_actions = []
 
-
 def set_position(entity, point):
    entity.position = point
 
 def get_position(entity):
    return entity.position
-
 
 def get_images(entity):
    return entity.imgs
@@ -105,10 +155,8 @@ def get_images(entity):
 def get_image(entity):
    return entity.imgs[entity.current_img]
 
-
 def get_rate(entity):
    return entity.rate
-
 
 def set_resource_count(entity, n):
    entity.resource_count = n
@@ -116,22 +164,17 @@ def set_resource_count(entity, n):
 def get_resource_count(entity):
    return entity.resource_count
 
-
 def get_resource_limit(entity):
    return entity.resource_limit
-
 
 def get_resource_distance(entity):
    return entity.resource_distance
 
-
 def get_name(entity):
    return entity.name
 
-
 def get_animation_rate(entity):
    return entity.animation_rate
-
 
 def remove_pending_action(entity, action):
    if hasattr(entity, "pending_actions"):
@@ -140,7 +183,6 @@ def remove_pending_action(entity, action):
 def add_pending_action(entity, action):
    if hasattr(entity, "pending_actions"):
       entity.pending_actions.append(action)
-
 
 def get_pending_actions(entity):
    if hasattr(entity, "pending_actions"):
@@ -151,7 +193,6 @@ def get_pending_actions(entity):
 def clear_pending_actions(entity):
    if hasattr(entity, "pending_actions"):
       entity.pending_actions = []
-
 
 def next_image(entity):
    entity.current_img = (entity.current_img + 1) % len(entity.imgs)
