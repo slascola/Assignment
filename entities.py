6e8cc9b35e5a1fe7135f29f126471ff68099c17a
Stephanie <slascola@calpoly.edu>
2015-04-01 23:28:45 -0700

    Class BlackSmith added Methods from entities

--- entities.py
+++ entities.py
@@ -251,6 +251,58 @@ class Blacksmith:
       self.resource_distance = resource_distance
       self.pending_actions = []
 
+   def set_position(entity, point):
+      entity.position = point
+
+   def get_position(entity):
+      return entity.position
+
+   def get_images(entity):
+      return entity.imgs
+
+   def get_image(entity):
+      return entity.imgs[entity.current_img]
+
+   def get_rate(entity):
+      return entity.rate        
+
+   def set_resource_count(entity, n):              
+      entity.resource_count = n   
+
+   def get_resource_count(entity):
+      return entity.resource_count             
+
+   def get_resource_limit(entity):
+      return entity.resource_limit
+
+   def get_resource_distance(entity):
+      return entity.resource_distance
+
+   def get_name(entity): 
+      return entity.name     
+
+   def remove_pending_action(entity, action):
+      if hasattr(entity, "pending_actions"):
+         entity.pending_actions.remove(action)
+
+   def add_pending_action(entity, action):
+      if hasattr(entity, "pending_actions"):
+         entity.pending_actions.append(action)
+
+   def get_pending_actions(entity):
+      if hasattr(entity, "pending_actions"):
+         return entity.pending_actions
+      else:
+         return []
+
+   def clear_pending_actions(entity):
+      if hasattr(entity, "pending_actions"):
+         entity.pending_actions = []
+
+   def next_image(entity):
+      entity.current_img = (entity.current_img + 1) % len(entity.imgs)
+
+
 class Obstacle:
    def __init__(self, name, position, imgs):
       self.name = name
