7f0300030747631cc7b1554e919069b20d72622f
slascola <stephlascola347@gmail.com>
2015-04-08 19:18:50 -0700

    Changed actions.py for ores into entities.py methods class

--- actions.py
+++ actions.py
@@ -32,12 +32,12 @@ def sign(x):
       return 0
 
 
-def adjacent(pt1, pt2):
+def adjacent(pt1, pt2): #function
    return ((pt1.x == pt2.x and abs(pt1.y - pt2.y) == 1) or
       (pt1.y == pt2.y and abs(pt1.x - pt2.x) == 1))
 
 
-def next_position(world, entity_pt, dest_pt):
+def next_position(world, entity_pt, dest_pt): #function
    horiz = sign(dest_pt.x - entity_pt.x)
    new_pt = point.Point(entity_pt.x + horiz, entity_pt.y)
 
@@ -177,7 +177,7 @@ def next_position(world, entity_pt, dest_pt):
    #return action
 
 
-def find_open_around(world, pt, distance):
+def find_open_around(world, pt, distance): #function
    for dy in range(-distance, distance + 1):
       for dx in range(-distance, distance + 1):
          new_pt = point.Point(pt.x + dx, pt.y + dy)
@@ -249,7 +249,7 @@ def find_open_around(world, pt, distance):
     #  return create_miner_full_action(world, entity, image_store)
 
 
-def create_animation_action(world, entity, repeat_count):
+def create_animation_action(world, entity, repeat_count): #function
    def action(current_ticks):
       entity.remove_pending_action(action)
 
@@ -264,38 +264,38 @@ def create_animation_action(world, entity, repeat_count):
    return action
 
 
-def create_entity_death_action(world, entity):
+def create_entity_death_action(world, entity): #function?
    def action(current_ticks):
       entity.remove_pending_action(action)
       pt = entity.get_position()
-      remove_entity(world, entity)
+      remove_entity(entity, world)
       return [pt]
    return action
 
 
-def create_ore_transform_action(world, entity, i_store): #ore
-   def action(current_ticks):
-      entity.remove_pending_action(action)
-      blob = create_blob(world, entity.get_name() + " -- blob",
-         entity.get_position(),
-         entity.get_rate() // BLOB_RATE_SCALE,
-         current_ticks, i_store)
+#def create_ore_transform_action(world, entity, i_store): #ore
+ #  def action(current_ticks):
+  #    entity.remove_pending_action(action)
+   #   blob = create_blob(world, entity.get_name() + " -- blob",
+    #     entity.get_position(),
+     #    entity.get_rate() // BLOB_RATE_SCALE,
+      #   current_ticks, i_store)
 
-      remove_entity(world, entity)
-      world.add_entity(blob)
+    #  remove_entity(world, entity)
+     # world.add_entity(blob)
 
-      return [blob.get_position()]
-   return action
+      #return [blob.get_position()]
+   #return action
 
 
-def remove_entity(world, entity):
+def remove_entity(entity, world): #world?
    for action in entity.get_pending_actions():
       world.unschedule_action(action)
    entity.clear_pending_actions()
    world.remove_entity(entity)
 
 
-def create_blob(world, name, pt, rate, ticks, i_store):
+def create_blob(world, name, pt, rate, ticks, i_store): #world?
    blob = entities.OreBlob(name, pt, rate,
       image_store.get_images(i_store, 'blob'),
       random.randint(BLOB_ANIMATION_MIN, BLOB_ANIMATION_MAX)
@@ -316,34 +316,34 @@ def create_blob(world, name, pt, rate, ticks, i_store):
    #schedule_animation(miner, world)
 
 
-def create_ore(world, name, pt, ticks, i_store):
+def create_ore(world, name, pt, ticks, i_store): #world?
    ore = entities.Ore(name, pt, image_store.get_images(i_store, 'ore'),
       random.randint(ORE_CORRUPT_MIN, ORE_CORRUPT_MAX))
-   schedule_ore(world, ore, ticks, i_store)
+   ore.schedule_ore(world, ticks, i_store)
 
    return ore
 
 
-def schedule_ore(world, ore, ticks, i_store): #ore
-   schedule_action(ore, world,
-      create_ore_transform_action(world, ore, i_store),
-      ticks + ore.get_rate())
+#def schedule_ore(world, ore, ticks, i_store): #ore
+ #  schedule_action(ore, world,
+  #    create_ore_transform_action(world, ore, i_store),
+   #   ticks + ore.get_rate())
 
 
-def create_quake(world, pt, ticks, i_store):
+def create_quake(world, pt, ticks, i_store): #world?
    quake = entities.Quake("quake", pt,
       image_store.get_images(i_store, 'quake'), QUAKE_ANIMATION_RATE)
    schedule_quake(world, quake, ticks)
    return quake
 
 
-def schedule_quake(world, quake, ticks): #ore
+def schedule_quake(world, quake, ticks): #quake
    schedule_animation(quake, world, QUAKE_STEPS)
    schedule_action(quake, world, create_entity_death_action(world, quake),
       ticks + QUAKE_DURATION)
 
 
-def create_vein(world, name, pt, ticks, i_store):
+def create_vein(world, name, pt, ticks, i_store): #world?
    vein = entities.Vein("vein" + name,
       random.randint(VEIN_RATE_MIN, VEIN_RATE_MAX),
       pt, image_store.get_images(i_store, 'vein'))
@@ -355,18 +355,18 @@ def create_vein(world, name, pt, ticks, i_store):
   #    ticks + vein.get_rate())
 
 
-def schedule_action(entity, world, action, time):
+def schedule_action(entity, world, action, time): #function
    entity.add_pending_action(action)
    world.schedule_action(action, time)
 
 
-def schedule_animation(entity, world, repeat_count=0):
+def schedule_animation(entity, world, repeat_count=0): #function
    schedule_action(entity, world,
       create_animation_action(world, entity, repeat_count),
       entity.get_animation_rate())
 
 
-def clear_pending_actions(entity, world):
+def clear_pending_actions(entity, world): #function
    for action in entity.get_pending_actions():
       world.unschedule_action(action)
    entity.clear_pending_actions()
