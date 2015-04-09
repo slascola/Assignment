64b8a80cc476122598b331a674ae00d7f0ddc466
slascola <stephlascola347@gmail.com>
2015-04-08 17:45:10 -0700

    Added functions from actions in oreblob class changed accordingly

--- entities.py
+++ entities.py
@@ -1,4 +1,5 @@
 import point
+import actions
 
 class Background:
    def __init__(self, name, imgs):
@@ -377,6 +378,64 @@ class OreBlob:
 
    def next_image(self):
       self.current_img = (self.current_img + 1) % len(self.imgs)
+
+   def blob_to_vein(self, world, vein): #blob class with blob_next position
+      entity_pt = self.get_position()
+      if not vein:
+         return ([entity_pt], False)
+      vein_pt = vein.get_position()
+      if actions.adjacent(entity_pt, vein_pt):
+         actions.remove_entity(world, vein)
+         return ([vein_pt], True)
+      else:
+         new_pt = self.blob_next_position(world, entity_pt, vein_pt)
+         old_entity = world.get_tile_occupant(new_pt)
+         if isinstance(old_entity, Ore):
+            actions.remove_entity(world, old_entity)
+         return (world.move_entity(self,new_pt), False)
+
+   def blob_next_position(self, world, entity_pt, dest_pt): #blobs with blob to vein
+      horiz = actions.sign(dest_pt.x - entity_pt.x)
+      new_pt = point.Point(entity_pt.x + horiz, entity_pt.y)
+
+      if horiz == 0 or (world.is_occupied(new_pt) and
+         not isinstance(world.get_tile_occupant(new_pt),
+         Ore)):
+         vert = actions.sign(dest_pt.y - entity_pt.y)
+         new_pt = point.Point(entity_pt.x, entity_pt.y + vert)
+
+         if vert == 0 or (world.is_occupied(new_pt) and
+            not isinstance(world.get_tile_occupant(new_pt),
+            Ore)):
+            new_pt = point.Point(entity_pt.x, entity_pt.y)
+
+      return new_pt
+
+   def create_ore_blob_action(self, world, i_store): #oreblob class
+      def action(current_ticks):
+         self.remove_pending_action(action)
+
+         entity_pt = self.get_position()
+         vein = world.find_nearest(entity_pt, Vein)
+         (tiles, found) = self.blob_to_vein(world, vein)
+
+         next_time = current_ticks + self.get_rate()
+         if found:
+            quake = actions.create_quake(world, tiles[0], current_ticks, i_store)
+            world.add_entity(quake)
+            next_time = current_ticks + self.get_rate() * 2
+
+         actions.schedule_action(self, world,
+            self.create_ore_blob_action(world, i_store),
+            next_time)
+
+         return tiles
+      return action
+
+   def schedule_blob(self, world, ticks, i_store):
+      actions.schedule_action(self,world, self.create_ore_blob_action(world, i_store),
+         ticks + self.get_rate())
+      actions.schedule_animation(self, world)
 
 
 class Quake:
